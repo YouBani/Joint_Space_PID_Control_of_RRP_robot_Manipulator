13e2d5e43faf145520cd03ccd32b913f1e5c5355
Youness Bani <91743838+YouBani@users.noreply.github.com>
2021-12-13 19:14:38 +0100

    Create rrp_position_controller.py

---
+++ rbe500_project /scripts/rrp_position_controller.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+import rospy
+import csv
+import time
+# from gazebo_msgs.srv import *
+from sensor_msgs.srv import *
+# from rbe500_project.msg import joint_angles 
+from std_msgs.msg import Float64
+from rbe500_project.srv import rrpIK, rrpIKResponse
+from geometry_msgs.msg import Pose, Point, Quaternion, Vector3
+from rrp_ik_server import inverse_kinematics
+
+
+def test():
+    
+    case1 = [0, 0.77, 0.34]
+    case2 = [-0.345, 0.425, 0.24]
+    case3 = [-0.67, -0.245, 0.14]
+    case4 = [0.77, 0.0, 0.39]
+    case5 = [0.425, 0.344, 0.29]
+    test_case = case2
+    joint_angle = inverse_kinematics(test_case)
+
+def get_position(joint):
+    # self.position_sub = rospy.Subscriber("/rrp/joint_states", JointState, self.rrp_callback)
+    try:
+        joint_call = rospy.Subscriber("/rrp/joint_states", JointState, callback)
+        joint_data = joint_call(joint)
+        position = joint_data.position[0]
+        return position
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Action terminated.")
+
+if __name__ == "__main__":
+    whatever = test()
+# joint_names = 'theta1', 'theta2', 'd3'
+
+# Kp_vals = [25, 70, 40]
+# Kd_vals = [25, 10, 20]
+
+# # 10Hz frequency
+# ros_rate = 10.0
+
+# # Loop execution rate in seconds (1/freq)
+# sampling_rate = (1/ros_rate) 
+# last_positions = [0.0, 0.0, 0.0]
+# last_set_points = [0.0, 0.0, 0.0]
+
+# # Record data every 15 seconds
+# record_time_interval = 15
+
+# start_time = 0
+# timer_started = False
+# times = []
+# set_points = []
+# curr_points = []
+
+# def reset_timer():
+#     global times, set_points, curr_points, timer_started
+
+#     times = []
+#     set_points = []
+#     curr_points = []
+#     timer_started = False
+
+# def record_data(time, set_point, curr_point):
+
+#     with open('position_controller_data.csv', 'wb') as csv_file:
+
+#         writer = csv.writer(csv_file, delimiter=',')
+
+#         for i in range(len(time)):
+#             writer.writerow((time[i], set_point[i], curr_point[i]))
+
+
+# def write_effort(effort, duration_sec, joint_name):
+#     rospy.wait_for_service('/gazebo/apply_joint_effort')
+
+#     try:
+#         joint_call = rospy.ServiceProxy('/gazebo/apply_joint_effort', ApplyJointEffort)
+#         joint_req = ApplyJointEffortRequest()
+#         joint_req.joint_name = joint_name
+#         joint_req.effort = effort
+#         joint_req.duration.nsecs = duration_sec * 100000000 #Convert sampling rate to nanoseconds
+#         res = joint_call(joint_req)
+#     except rospy.ServiceException as e:
+#         print("Service call failed: %s"%e)
+
+
+# def get_position(joint):
+#     # self.position_sub = rospy.Subscriber("/rrp/joint_states", JointState, self.rrp_callback)
+#     try:
+#         joint_call = rospy.Subscriber("/rrp/joint_states", JointState, callback)
+#         joint_data = joint_call(joint)
+#         position = joint_data.position[0]
+#         return position
+#     except rospy.ROSInterruptException:
+#         rospy.loginfo("Action terminated.")
+
+
+# def do_pd_control(set_point, curr_point, joint_name):
+#     global last_positions, last_set_points, timer_started, times, set_points, curr_points, start_time
+
+#     # Start timer for recording data
+#     if not timer_started:
+#         start_time = time.time()
+#         timer_started = True
+
+#     # Calculate errors
+#     position_err = set_point - curr_point
+
+#     if (joint_name is 'theta1'):
+
+#         derivative_err = (last_positions[0] - curr_point)/sampling_rate
+
+#         # Set reference vals
+#         last_positions[0] = curr_point
+#         last_set_points[0] = set_point
+
+#         # PD output
+#         effort = position_err*Kp_vals[0] + derivative_err*Kd_vals[0]
+#     elif (joint_name is 'theta2'):
+
+#         derivative_err = (last_positions[1] - curr_point)/sampling_rate
+
+#         # Set reference vals
+#         last_positions[1] = curr_point
+#         last_set_points[1] = set_point
+
+#         # PD output
+#         effort = position_err*Kp_vals[1] + derivative_err*Kd_vals[1]
+#     elif (joint_name is 'd3'):
+
+#         derivative_err = (last_positions[2] - curr_point)/sampling_rate
+
+#         # Set reference vals
+#         last_positions[2] = curr_point
+#         last_set_points[2] = set_point
+
+#         # PD output
+#         effort = position_err*Kp_vals[2] + derivative_err*Kd_vals[2]
+
+#     # Append data entries to lists
+#     times.append(time.time())
+#     set_points.append(set_point)
+#     curr_points.append(curr_point)
+
+#     # Write effort to joint
+#     write_effort(effort, sampling_rate, joint_name)
+
+#     # If 15 secs have passed, record the data to a CSV file and reset timer params
+#     if time.time() > (start_time + record_time_interval):
+#         rospy.loginfo("Recorded data")
+#         record_data(times, set_points, curr_points)
+#         reset_timer()
+
+
+# def callback(msg):
+#     theta1 = get_position(joint_names[0])
+#     theta2 = get_position(joint_names[1])
+#     d3 = get_position(joint_names[2])
+
+#     do_pd_control(msg.theta1, theta1, joint_names[0])
+#     do_pd_control(msg.theta2, theta2, joint_names[1])
+#     do_pd_control(msg.d3, d3, joint_names[2])
+
+# def main():
+#     rospy.init_node("position_controller")
+
+#     sub = rospy.Subscriber("/scara/set_points", joint_angles, callback)
+#     r = rospy.Rate(ros_rate)
+
+#     while not rospy.is_shutdown():
+#         r.sleep()
+#         do_pd_control(last_set_points[0], get_position(joint_names[0]), joint_names[0]) # maintain theta1 position
+#         do_pd_control(last_set_points[1], get_position(joint_names[1]), joint_names[1]) # maintain theta2 position
+#         do_pd_control(last_set_points[2], get_position(joint_names[2]), joint_names[2]) # maintain d3 position       
+
+# if __name__ == "__main__":
+#     try:
+#         main()
+#     except rospy.ROSInterruptException:
+#         pass
