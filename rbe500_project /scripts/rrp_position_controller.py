0c780577b1032616531e425a922924b468bf286d
Youness Bani <91743838+YouBani@users.noreply.github.com>
2021-12-14 23:55:27 +0100

    controller to fix

--- rbe500_project /scripts/rrp_position_controller.py
+++ rbe500_project /scripts/rrp_position_controller.py
@@ -28,11 +28,12 @@ joint_angle = inverse_kinematics(test_case)
 
 joint_names = 'theta1', 'theta2', 'd3'
 
-Kp_vals = [25, 70, 40]
-Kd_vals = [25, 10, 20]
+Kp_vals = [1.5, 2.0, 3.0]
+Ki_vals = [0.05, 0.03, 1.5]
+Kd_vals = [2, 1000, 1000]
 
-# 10Hz frequency
-ros_rate = 10.0
+# 100Hz frequency
+ros_rate = 100.0
 
 # Loop execution rate in seconds (1/freq)
 sampling_rate = (1/ros_rate) 
@@ -53,55 +54,53 @@ def reset_timer():
 
     times = []
     set_points = []
-    curr_points = []
+    curr_point = []
     timer_started = False
 
-curr_theta1 = 0
-curr_theta2 = 0
-curr_d3 = 0
+# curr_theta1 = 0
+# curr_theta2 = 0
+# curr_d3 = 0
 
 def call_back(data):
     global curr_theta1, curr_theta2, curr_d3
     curr_theta1 = data.position[0]
     curr_theta2 = data.position[1]
     curr_d3 = data.position[2]
-
+    print(curr_theta1)
     # control(joint_angle.joint_vals.theta1, curr_theta1, joint_names[0])
     # control(joint_angle.joint_vals.theta2, curr_theta2, joint_names[1])
     # control(joint_angle.joint_vals.d3, curr_d3, joint_names[2])
 
 
 def get_position():
+    rate_value = 100
     rospy.init_node("listner", anonymous=True)
-    
-    # joint_call = rospy.Subscriber("/rrp/joint_states", JointState, joint_state_callback)
     joint_call = rospy.Subscriber("/rrp/joint_states", JointState, call_back)
     # joint_data = joint_call(joint)
     # rospy.Rate(10)
-    rospy.spin()
-    # position = joint_state.position
-    # print("theta1, theta2, d3: ", position)
-    # return position
-    # except rospy.ROSInterruptException:
-    #     rospy.loginfo("Action terminated.")
+    rate = rospy.Rate(rate_value)
+
+    rate.sleep()
 
 
 
 def joint_effort(effort, jointN):
-    rate_value = 10
-    rospy.init_node('joint_ctrl', anonymous=True)
-    rate = rospy.Rate(rate_value) # 10h
+    rate_value = 100
+    # rospy.init_node('joint_ctrl', anonymous=True)
+    rate = rospy.Rate(rate_value) # 100h
     pub = rospy.Publisher('/rrp/'+jointN+'_effort_controller/command', Float64, queue_size=1)
-    while not rospy.is_shutdown():
-        try:  
-            pub.publish(effort)
-            rate.sleep()
-        except rospy.ROSInterruptException:
-	        rospy.logerr("ROS Interrupt Exception! Just ignore the exception!")
-        except rospy.ROSTimeMovedBackwardsException:
-	        rospy.logerr("ROS Time Backwards! Just ignore the exception!") 
+    
+    # while not rospy.is_shutdown():
+    # while count <100:
+    try:  
+        pub.publish(effort)
+        rate.sleep()
+    except rospy.ROSInterruptException:
+        rospy.logerr("ROS Interrupt Exception! Just ignore the exception!")
+    except rospy.ROSTimeMovedBackwardsException:
+        rospy.logerr("ROS Time Backwards! Just ignore the exception!") 
 
-
+    # pub.publish(0)
 
 
 def control(set_point, curr_point, joint_name):
@@ -114,45 +113,52 @@ def control(set_point, curr_point, joint_name):
 
     # Calculate errors
     position_err = set_point - curr_point
-
+    totalErrorJ1 = 0
+    totalErrorJ2 = 0
+    totalErrorJ3 = 0
+    print("position_err", position_err)
+    # while position_err > 0.10:
     if (joint_name is 'theta1'):
         
-
+        print(position_err)
         derivative_err = (last_positions[0] - curr_point)/sampling_rate
+        totalErrorJ1 = (totalErrorJ1 + position_err)*sampling_rate
 
         # Set reference vals
         last_positions[0] = curr_point
         last_set_points[0] = set_point
 
-        # PD output
-        effort = position_err*Kp_vals[0] + derivative_err*Kd_vals[0]
+        # PID output
+        effort = position_err*Kp_vals[0] + totalErrorJ1*Ki_vals[0] + derivative_err*Kd_vals[0]
+        # print("effort loop: ", effort)
         
     elif (joint_name is 'theta2'):
 
         derivative_err = (last_positions[1] - curr_point)/sampling_rate
-
+        totalErrorJ2 = (totalErrorJ2 + position_err)*sampling_rate
         # Set reference vals
         last_positions[1] = curr_point
         last_set_points[1] = set_point
 
-        # PD output
-        effort = position_err*Kp_vals[1] + derivative_err*Kd_vals[1]
+        # PID output
+        effort = position_err*Kp_vals[1] + totalErrorJ2*Ki_vals[1] + derivative_err*Kd_vals[1]
     elif (joint_name is 'd3'):
 
         derivative_err = (last_positions[2] - curr_point)/sampling_rate
+        totalErrorJ3 = (totalErrorJ3 + position_err)*sampling_rate
 
         # Set reference vals
         last_positions[2] = curr_point
         last_set_points[2] = set_point
+        
+        # PID output
+        effort = position_err*Kp_vals[2] + totalErrorJ3*Ki_vals[2] + derivative_err*Kd_vals[2]
+        # Append data entries to lists
+        times.append(time.time())
+        set_points.append(set_point)
+        curr_points.append(curr_point)
 
-        # PD output
-        effort = position_err*Kp_vals[2] + derivative_err*Kd_vals[2]
-
-    # Append data entries to lists
-    times.append(time.time())
-    set_points.append(set_point)
-    curr_points.append(curr_point)
-    print("effort", effort)
+    # print("effort", effort)
 
     if joint_name == "theta1":
         joint_effort(effort, "joint1")
@@ -162,37 +168,22 @@ def control(set_point, curr_point, joint_name):
         joint_effort(effort, "joint3")
 
 
-def joint_state_callback(msg):
-    joint_state = JointState()
-    joint_state_position = msg.position
-    joint_state_velocity = msg.velocity
-    joint_state_name = msg.name
-
-    theta1 = joint_state_position[0]
-    theta2 = joint_state_position[1]
-    d3 = joint_state_position[2]
-    print(theta1, theta2, d3)
-    # do_pd_control(set_point, curr_point, joint_name):
-    control(joint_angle.joint_vals.theta1, theta1, joint_names[0])
-    control(joint_angle.joint_vals.theta2, theta2, joint_names[1])
-    control(joint_angle.joint_vals.d3, d3, joint_names[2])
 
 
 if __name__ == "__main__":
     try:
-        # get_position()
-        # print(curr_theta1, curr_theta2, curr_d3)
-        control(joint_angle.joint_vals.theta1, curr_theta1, "theta1")
-        # joint_effort(2, "joint1")
-        # theta1 = get_position()[0]
-        # theta1 = joint_state_position[0]
-        # theta2 = joint_state_position[1]
-        # d3 = joint_state_position[2]
-        # control(last_set_points[0], get_position(joint_names[0]), joint_names[0])
-    # do_pd_control(set_point, curr_point, joint_name):
-        # control(joint_angle.joint_vals.theta1, theta1, joint_names[0])
-        # get_position()
-        # control(last_set_points[0], get_position()[0], joint_names[0]) 
-        # control(last_set_points[0], get_position(joint_names[0]), joint_names[0])
+        get_position()
+        # curr_theta1 = 0
+        # print("curr_theta1", curr_theta1)
+        # print("set point", joint_angle.joint_vals.theta1)
+        # print("joint_angle.joint_vals.theta1", joint_angle.joint_vals.theta1)
+        while joint_angle.joint_vals.theta1 - curr_theta1 > 0.15:
+            # print("the error", joint_angle.joint_vals.theta1 - curr_theta1)
+            control(joint_angle.joint_vals.theta1, curr_theta1, "theta1")
+            #control(joint_angle.joint_vals.theta2, theta2, joint_names[1])
+#           control(joint_angle.joint_vals.d3, d3, joint_names[2])
+
+        # joint_effort(10, "joint1")
+        # print(curr_theta1)
     except rospy.ROSInterruptException:
         pass
