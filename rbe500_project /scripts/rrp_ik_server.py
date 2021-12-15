6cd06828d2f65d843ac08b56a9a3785cfdb483e8
Youness Bani <91743838+YouBani@users.noreply.github.com>
2021-12-15 23:08:41 +0100

    Final changes

--- rbe500_project /scripts/rrp_ik_server.py
+++ rbe500_project /scripts/rrp_ik_server.py
@@ -21,67 +21,40 @@ def inverse_kinematics(req):
     l2 = 0.345
     l3 = 0.345 + 0.05
 
-
-    # Px = float(position[0])
-    # Py = float(position[1])
-    # Pz = float(position[2])
-    # Px = req.x
-    # Py = req.y
-    # Pz = req.z
+    # For using the inverse kinematic client 
     Px = float(req[0])
     Py = float(req[1])
     Pz = float(req[2])
-    # Px = 0.44
-    # d3 = l3 - Pz
-    
-    theta2 = acos((Px**2 + Py**2 - l1**2 - l2**2)/(2*l1*l2))
-    # C_theta2 = cos((Px**2 + Py**2 - l1**2 - l2**2)/(2*l1*l2))
-    # S_theta2 = sqrt(1 - C_theta2**2)
-    # theta2 = atan2(S_theta2,C_theta2)
-    theta1 = atan2(Py, Px) - atan2((l2+l1)*sin(theta2), l1+l2+cos(theta2))
-    # theta1 = atan2(Py, Px) - (l1 + l2 * cos(theta2))/sqrt(Px**2 + Py**2)
+    # For using the p1, p2, p3 and p4 lists 
+    # Px = req.x
+    # Py = req.y
+    # Pz = req.z
+
+    if Py==0:
+        if Px>0:
+            theta1 = 0
+            theta2 = 0
+        else:
+            theta2 = 0
+            theta1 = pi
+    elif Px == 0:
+        if Py>0:
+            theta1 = pi/2
+            theta2 = 0
+        else:
+            theta2 = 0
+            theta1 = -pi/2
+    else:
+        theta2 = acos((Px**2 + Py**2 - l1**2 - l2**2)/(2*l1*l2))
+        theta1 = atan2(Py, Px) - atan2((l2+l1)*sin(theta2), l1+l2+cos(theta2))
+        theta1 = abs(theta1)
     d3 = l3 - Pz
 
-#====#
-    # d3 = Pz
-    # cos_theta2 = acos(Px**2 + Py**2 - l1**2 - l2**2) / (2 * l1 * l2)  
-    # sin_theta2 = sqrt(1-(cos_theta2**2))
-
-    # theta2 = atan2(sin_theta2, cos_theta2)
-    # print("theta2", theta2)
-    # alpha = atan2(Py, Px)
-    # beta = (l1 + l2 * cos(theta2))/sqrt(Px**2 + Py**2)
-
-    # # alpha = math.atan2(y,x)
-    # # D1 = (x**2+y**2)/(2*L*math.sqrt(x**2+y**2))
-    # # C1 = math.sqrt(1-(D1**2))
-    # # beta = math.atan2(C1, D1)
-    
-    # theta1 = alpha - beta
-
-#===
-    # stheta2 = sqrt(1 - (ctheta2**2))
-    # print("stheta2", stheta2)
-    # theta2 = atan2(stheta2, ctheta2)
-    # theta2 = acos(ctheta2)
-    # print("theta2", theta2)
-    
-    # ctheta1  =  (px  *  (l1  +  l2  *  ctheta2)  +  py  *  l2  * stheta2) / (px**2 + py**2)  
-
-
-    # result.theta1 = joint1
-    # result.theta2 = theta2
-    # result.d3 = d3
-    # print(tetha1)
-    # print(theta2)
-    # print(d3)
-    # angles.x = theta1
-    # angles.y = theta2
-    # angles.z = d3
     result.theta1 = theta1  
     result.theta2 = theta2
     result.d3 = d3
     return rrpIKResponse(result)
+
 
 def inverse_kinematics_server():
     rospy.init_node('rrp_ik_server')
@@ -89,6 +62,7 @@ def inverse_kinematics_server():
     s = rospy.Service('inverse_kinematics', rrpIK , inverse_kinematics) 
     print("Ready to calculate the inverse kinematics")
     rospy.spin()
+
 
 if __name__ == "__main__":
     inverse_kinematics_server()
--- rbe500_project /scripts/rrp_position_controller.py
+++ rbe500_project /scripts/rrp_position_controller.py
@@ -11,29 +11,11 @@ from geometry_msgs.msg import Pose, Point, Quaternion, Vector3
 from rrp_ik_server import inverse_kinematics
 
 
-joint_names = ['theta1', 'theta2', 'd3']
-case1 = [0, 0.77, 0.34]
-case2 = [-0.345, 0.425, 0.24]
-case3 = [-0.67, -0.245, 0.14]
-case4 = [0.77, 0.0, 0.39]
-# case5 = [0.425, 0.344, 0.29]
-test_case = case3
-joint_angle = inverse_kinematics(test_case)
-# print(joint_angle.joint_vals.theta1)
-# print(joint_angle["joint_vals"])
-
-# rosrun rbe500_project rrp_position_controller.py
-joint_names = 'theta1', 'theta2', 'd3'
-
-Kp_vals = [3, 2, 200]
-Ki_vals = [0.01, 0.01, 0.01]
-Kd_vals = [15, 5, 0.05]
-
 # 100Hz frequency
 ros_rate = 100.0
 
 # Loop execution rate in seconds (1/freq)
-sampling_rate = (1/ros_rate) 
+sampling_rate = (1/ros_rate)
 last_positions = [0.0, 0.0, 0.0]
 last_set_points = [0.0, 0.0, 0.0]
 
@@ -46,6 +28,7 @@ times = []
 set_points = []
 curr_points = []
 
+
 def reset_timer():
     global times, set_points, curr_points, timer_started
 
@@ -54,15 +37,13 @@ def reset_timer():
     curr_point = []
     timer_started = False
 
+
 def call_back(data):
     global curr_theta1, curr_theta2, curr_d3
     curr_theta1 = data.position[0]
     curr_theta2 = data.position[1]
     curr_d3 = data.position[2]
     print(curr_theta1)
-    # control(joint_angle.joint_vals.theta1, curr_theta1, joint_names[0])
-    # control(joint_angle.joint_vals.theta2, curr_theta2, joint_names[1])
-    # control(joint_angle.joint_vals.d3, curr_d3, joint_names[2])
 
 
 def get_position():
@@ -73,24 +54,21 @@ def get_position():
     rate.sleep()
 
 
-
 def joint_effort(effort, jointN):
     rate_value = 100
     # rospy.init_node('joint_ctrl', anonymous=True)
     rate = rospy.Rate(rate_value) # 100h
     pub = rospy.Publisher('/rrp/'+jointN+'_effort_controller/command', Float64, queue_size=1)
-    
+
     # while not rospy.is_shutdown():
     # while count <100:
-    try:  
+    try:
         pub.publish(effort)
         rate.sleep()
     except rospy.ROSInterruptException:
         rospy.logerr("ROS Interrupt Exception! Just ignore the exception!")
     except rospy.ROSTimeMovedBackwardsException:
-        rospy.logerr("ROS Time Backwards! Just ignore the exception!") 
-
-    # pub.publish(0)
+        rospy.logerr("ROS Time Backwards! Just ignore the exception!")
 
 
 def control(set_point, curr_point, joint_name):
@@ -108,8 +86,8 @@ def control(set_point, curr_point, joint_name):
     totalErrorJ3 = 0
     print("position_err", position_err)
     # while position_err > 0.10:
-    if (joint_name is 'theta1'):
-        
+    if joint_name is 'theta1':
+
         print(position_err)
         derivative_err = (last_positions[0] - curr_point)/sampling_rate
         totalErrorJ1 = (totalErrorJ1 + position_err)*sampling_rate
@@ -121,8 +99,8 @@ def control(set_point, curr_point, joint_name):
         # PID output
         effort = position_err*Kp_vals[0] + totalErrorJ1*Ki_vals[0] + derivative_err*Kd_vals[0]
         # print("effort loop: ", effort)
-        
-    elif (joint_name is 'theta2'):
+
+    elif joint_name is 'theta2':
 
         derivative_err = (last_positions[1] - curr_point)/sampling_rate
         totalErrorJ2 = (totalErrorJ2 + position_err)*sampling_rate
@@ -132,7 +110,7 @@ def control(set_point, curr_point, joint_name):
 
         # PID output
         effort = position_err*Kp_vals[1] + totalErrorJ2*Ki_vals[1] + derivative_err*Kd_vals[1]
-    elif (joint_name is 'd3'):
+    elif joint_name is 'd3':
 
         derivative_err = (last_positions[2] - curr_point)/sampling_rate
         totalErrorJ3 = (totalErrorJ3 + position_err)*sampling_rate
@@ -140,7 +118,7 @@ def control(set_point, curr_point, joint_name):
         # Set reference vals
         last_positions[2] = curr_point
         last_set_points[2] = set_point
-        
+
         # PID output
         effort = position_err*Kp_vals[2] + totalErrorJ3*Ki_vals[2] + derivative_err*Kd_vals[2]
         # Append data entries to lists
@@ -158,24 +136,51 @@ def control(set_point, curr_point, joint_name):
         joint_effort(effort, "joint3")
 
 
-
-
 if __name__ == "__main__":
-    try:
-        get_position()
-        while joint_angle.joint_vals.theta1 - curr_theta1 > 0.15:
-            # print("the error", joint_angle.joint_vals.theta1 - curr_theta1)
-            control(joint_angle.joint_vals.theta1, curr_theta1, "theta1")
-        joint_effort(0, "joint1")
+    # works
+    # Kp_vals = [5, 3, 450]
+    # Ki_vals = [0.01, 0.01, 0.01]
+    # Kd_vals = [17, 5, 0.05]
 
-        while joint_angle.joint_vals.theta2 - curr_theta2 > 0.15:
-            control(joint_angle.joint_vals.theta2, curr_theta2, "theta2")
-        joint_effort(0, "joint2")
-        while joint_angle.joint_vals.d3 - curr_d3 > 0.002:
-            control(joint_angle.joint_vals.d3, curr_d3, "d3")
-        joint_effort(0, "joint3")
-        rospy.sleep(1)
+    # Kp_vals = [5, 3, 450]
+    # Ki_vals = [0.01, 0.1, 0.01]
+    # Kd_vals = [17, 6, 0.05]
 
+    # Kp_vals = [12, 4, 450]
+    # Ki_vals = [0.001, 0.001, 0.01]
+    # Kd_vals = [14, 10, 0.05]
 
-    except rospy.ROSInterruptException:
-        pass
+    Kp_vals = [7, 3, 450]
+    Ki_vals = [0.01, 0.01, 0.01]
+    Kd_vals = [17, 5, 0.05]
+
+    case1 = [0, 0.77, 0.34]
+    case2 = [-0.345, 0.425, 0.24]
+    case3 = [-0.67, -0.245, 0.14]
+    case4 = [0.77, 0.0, 0.39]
+    test_cases = [case1, case2, case3, case4]
+    # test_cases = [case3, case4]
+    # test_cases = [case2]
+    for test_case in test_cases:
+        joint_angle = inverse_kinematics(test_case)
+        try:
+            get_position()
+            while abs(joint_angle.joint_vals.theta1 - curr_theta1) > 0.15:
+                # print("the error", joint_angle.joint_vals.theta1 - curr_theta1)
+                control(joint_angle.joint_vals.theta1, curr_theta1, "theta1")
+                # joint_effort(0, "joint2")
+            joint_effort(0, "joint1")
+
+            while abs(joint_angle.joint_vals.theta2 - curr_theta2) > 0.15:
+                # joint_effort(0, "joint1")
+                control(joint_angle.joint_vals.theta2, curr_theta2, "theta2")
+                joint_effort(0, "joint1")
+            joint_effort(0, "joint2")
+
+            while joint_angle.joint_vals.d3 - curr_d3 > 0.002:
+                control(joint_angle.joint_vals.d3, curr_d3, "d3")
+            joint_effort(0, "joint3")
+            rospy.sleep(1)
+
+        except rospy.ROSInterruptException:
+            pass
